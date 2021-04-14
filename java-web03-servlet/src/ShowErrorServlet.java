874647a0ff3efad6af2af4d89ed482e6cbaf5a05
mk2019 <unicorn.mark2019@gmail.com>
2021-04-14 22:59:44 +0800

    practice: response error code & page

---
+++ java-web03-servlet/src/ShowErrorServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 状态码 及 提示信息
+ */
+public class ShowErrorServlet extends HttpServlet {
+	//处理 get 方法
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		response.sendError(407,"请求要求代理的身份认证");
+	}
+	//处理 post 方法
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		this.doGet(request, response);
+	}
+}
